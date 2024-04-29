ae4c0bd1e0e055febbce8955deb7c6832723478d
muhamed-droid <mmasnopita1@etf.unsa.ba>
2024-04-29 13:21:00 +0200

    Minor modificaitons

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,19 @@
 import './App.css';
 import {BrowserRouter, Route, Routes, Link} from "react-router-dom";
 import Home from './pages/Home'; 
-import CreateProduct from './pages/CreateProduct';
-import Product from './pages/Product';
+import CreateVehicle from './pages/CreateVehicle';
+import Vehicle from './pages/Vehicle';
 
 function App() {
 
   return (
     <div className="App">
       <BrowserRouter> 
-        <Link to="/createproduct"> Create a Product</Link>
+        <Link to="/createvehicle"> Create a Product</Link>
           <Routes>
             <Route path="/" element={<Home/>}/>
-            <Route path="/createproduct" element={<CreateProduct/>}/>
-            <Route path="/product/:id" element={<Product/>}/>
+            <Route path="/createvehicle" element={<CreateVehicle/>}/>
+            <Route path="/vehicle/:id" element={<Vehicle/>}/>
           </Routes>
       </BrowserRouter>
     </div>
--- server/routes/Vehicles.js
+++ server/routes/Vehicles.js
@@ -5,19 +5,19 @@ const {Vehicles} = require("../models");
 
 router.get("/", async (req, res) => {
     const listOfVehicles = await Vehicles.findAll();
-    res.json(listOfProducts);
+    res.json(listOfVehicles);
 });
 
 router.get('/byId/:id', async (req, res) => {
     const id = req.params.id;
     const vehicle = await Vehicle.findByPk(id);
-    res.json(product);
+    res.json(vehicle);
 })
 
 router.post("/", async (req, res) => {
-   const product = req.body;
-    await Products.create(product);
-    res.json(product);
+   const vehicle = req.body;
+    await Products.create(vehicle);
+    res.json(vehicle);
 });
 
 module.exports = router;
