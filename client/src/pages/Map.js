2945922535d10920b7d6edcbb69da8fbd4e9fe6a
muhamed-droid <mmasnopita1@etf.unsa.ba>
2024-05-06 22:19:05 +0200

    map styled

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './css/Map.css';
 
 function Map() {
   return (
-    <div>
-      <iframe src="https://www.google.com/maps/d/u/2/embed?mid=1BhuJYrcAV5-ig5shwIpz-p6DsenFm30&ehbc=2E312F&noprof=1"
-         width="75%" 
-         height="650">
-        </iframe>
+    <div className="map-frame">
+      <div className="map-header">
+        <h2>Unsere Lokation</h2>
+        <p>Italiener Straße 70, Villach, Österreich</p>
+      </div>
+      <div className="map-container">
+        <iframe
+          src="https://www.google.com/maps/d/u/2/embed?mid=1BhuJYrcAV5-ig5shwIpz-p6DsenFm30&ehbc=2E312F&noprof=1"
+          width="100%"
+          height="500"
+          style={{ border: 'none' }}
+          loading="lazy"
+        ></iframe>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Map
+export default Map;
