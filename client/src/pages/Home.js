19f5cf80def97a9d79226e85794fff063b65e134
ezukic2 <ezukic2@etf.unsa.ba>
2024-05-08 19:12:41 +0200

    fix vehicles and form

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -48,9 +48,6 @@ function Home() {
             <img className="slika" src={`photos/${selectedVehicle.image_name}`} alt="Vehicle" />
           </Modal>
         )}
-        <div className='form'>
-          <Form></Form>
-        </div>
         <div>
           <ButtonToTop/>
         </div>
