e9f7114393722c93081c1b0c5a3fc9f4989c1622
muhamed-droid <mmasnopita1@etf.unsa.ba>
2024-05-08 20:57:19 +0200

    change

--- client/src/pages/Vehicles.js
+++ client/src/pages/Vehicles.js
@@ -1,33 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
-import './css/Vehicles.css'; // Provjeri da li je putanja ispravna
+import './css/Vehicles.css'; // Make sure the path is correct
 import Form from './Form.js';
 
 function Vehicles({ onVehicleClick }) {
   const [vehiclesState, setVehiclesState] = useState([]);
   const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' });
   const [filter, setFilter] = useState('');
-  const [modelFilter, setModelFilter] = useState('');
   const [filteredVehicles, setFilteredVehicles] = useState([]);
   const [showSortOptions, setShowSortOptions] = useState(false);
-  const [selectedSort, setSelectedSort] = useState(null);
-  const [selectedSortKey, setSelectedSortKey] = useState(null);
 
+  // Fetch vehicles data
   useEffect(() => {
-    axios.get(`http://localhost:3001/vehicles`).then((response) => {
-      setVehiclesState(response.data);
-    });
+    axios.get(`http://localhost:3001/vehicles`)
+      .then((response) => {
+        setVehiclesState(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the vehicles data:', error);
+      });
   }, []);
 
+  // Filter and sort vehicles
   useEffect(() => {
+    let updatedVehicles = [...vehiclesState];
+
+    // Filter based on vehicle type
     if (filter === 'LKW') {
-      setFilteredVehicles(vehiclesState.filter(vehicle => vehicle.lkw === true));
+      updatedVehicles = updatedVehicles.filter(vehicle => vehicle.lkw === true);
     } else if (filter === 'PKW') {
-      setFilteredVehicles(vehiclesState.filter(vehicle => vehicle.lkw === false));
-    } else {
-      setFilteredVehicles(vehiclesState);
+      updatedVehicles = updatedVehicles.filter(vehicle => vehicle.lkw === false);
     }
-  }, [filter, vehiclesState]);
+
+    // Sort vehicles
+    if (sortConfig.key) {
+      updatedVehicles.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+
+    setFilteredVehicles(updatedVehicles);
+  }, [filter, sortConfig, vehiclesState]);
 
   function handleModelFilterChange(event) {
     setFilter(event.target.value);
@@ -40,56 +59,51 @@ function Vehicles({ onVehicleClick }) {
   const sortBy = (key, direction) => {
     setSortConfig({ key, direction });
     setShowSortOptions(false);
-    setSelectedSort(`${key} (${direction})`);
-    setSelectedSortKey(null);
   };
 
   return (
     <div className='vozila'>
-      <div className="title-vozila"> Wagen </div>
+      <div className="title-vozila">Wagen</div>
       <div className="vehicle-form">
-
-      <div className="left-content">
-        <div className="vehicle-cards">
-          {filteredVehicles.map((value, key) => (
-            <div className="vehicle-card" onClick={() => onVehicleClick(value)} key={key}>
-              <div className="name">{value.manufacturer}</div>
-              <div className="model">{value.model}</div>
-              <div className="description">{value.description}</div>
-              <div className="price">{value.price} €/tag</div>
-              <img src={`./photos/${value.image_name}`} alt="vehicle" className="vehicle-image" />
-            </div>
-          ))}
+        <div className="left-content">
+          <div className="vehicle-cards">
+            {filteredVehicles.map((value) => (
+              <div className="vehicle-card" onClick={() => onVehicleClick(value)} key={value.id}>
+                <div className="name">{value.manufacturer}</div>
+                <div className="model">{value.model}</div>
+                <div className="description">{value.description}</div>
+                <div className="price">{value.price} €/tag</div>
+                <img src={`./photos/${value.image_name}`} alt="vehicle" className="vehicle-image" />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="right-content">
+          <div className="filter">
+            <select id="model" onChange={handleModelFilterChange}>
+              <option value="">All Models</option>
+              <option value="LKW">LKW</option>
+              <option value="PKW">PKW</option>
+            </select>
+          </div>
+          <div className="sort-container">
+            <button className="sort-button" onClick={toggleSortOptions}>Sort by</button>
+            {showSortOptions && (
+              <div className="sort-options">
+                <button className="sort-option" onClick={() => sortBy('price', 'ascending')}>Price (Ascending)</button>
+                <button className="sort-option" onClick={() => sortBy('price', 'descending')}>Price (Descending)</button>
+                <button className="sort-option" onClick={() => sortBy('manufacturer', 'ascending')}>Name (Ascending)</button>
+                <button className="sort-option" onClick={() => sortBy('manufacturer', 'descending')}>Name (Descending)</button>
+                <button className="sort-option" onClick={() => sortBy('year', 'ascending')}>Year (Ascending)</button>
+                <button className="sort-option" onClick={() => sortBy('year', 'descending')}>Year (Descending)</button>
+              </div>
+            )}
+          </div>
+          <div className='form'>
+            <Form />
+          </div>
         </div>
       </div>
-      <div className="right-content">
-        <div className="filter">
-          <select id="model" onChange={handleModelFilterChange}>
-            <option value="">All Models</option>
-            <option value="LKW">LKW</option>
-            <option value="PKW">PKW</option>
-          </select>
-        </div>
-        <div className="sort-container">
-          <button className="sort-button" onClick={toggleSortOptions}>Sort by</button>
-          {showSortOptions && (
-            <div className="sort-options">
-              <button className="sort-option" onClick={() => sortBy('price', 'ascending')}>Price (Ascending)</button>
-              <button className="sort-option" onClick={() => sortBy('price', 'descending')}>Price (Descending)</button>
-              <button className="sort-option" onClick={() => sortBy('manufacturer', 'ascending')}>Name (Ascending)</button>
-              <button className="sort-option" onClick={() => sortBy('manufacturer', 'descending')}>Name (Descending)</button>
-              <button className="sort-option" onClick={() => sortBy('year', 'ascending')}>Year (Ascending)</button>
-              <button className="sort-option" onClick={() => sortBy('year', 'descending')}>Year (Descending)</button>
-            </div>
-          )}
-        </div>
-        <div className='form'>
-          <Form />
-        </div>
-      </div>
-
-      </div>
-      
     </div>
   );
 }
