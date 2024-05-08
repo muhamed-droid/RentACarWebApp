6e996111ccd788d8ffd15eaa93aff96e8291cd5a
fmatoruga2 <fmatoruga2@etf.unsa.ba>
2024-05-08 12:42:05 +0200

    fsa

--- client/src/pages/FAQ.js
+++ client/src/pages/FAQ.js
@@ -3,7 +3,7 @@ import './css/FAQ.css'; // Stilizacija može biti u odvojenom CSS fajlu
 
 const FAQ = () => {
   const [faqs, setFaqs] = useState([
-    { question: 'What is Lorem Ipsum?', answer: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' },
+    { question: 'What sisfasfjas Lorem Ipsum?', answer: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' },
     { question: 'Why do we use it?', answer: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.' },
     { question: 'Where does it come from?', answer: 'Contrary to popular belief, Lorem Ipsum is not simply random text.' },
     { question: 'Where can I get some?', answer: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.' }
