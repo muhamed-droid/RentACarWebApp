43019a0e94afdf69ba0cb8b8ed441643479779d8
muhamed-droid <mmasnopita1@etf.unsa.ba>
2024-05-13 12:56:32 +0200

    navbar and home modifications

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -1,36 +1,40 @@
-import { FaPhone, FaEnvelope } from 'react-icons/fa'; // Import icons
+import React, { useState } from 'react';
+import { FaPhone, FaEnvelope, FaBars } from 'react-icons/fa'; // Import hamburger menu icon
+import ScrollToTopLink from './ScrollToTopLink.js';
+import Logo from '../photos/logo.png';
+import './css/Navbar.css';
 
-import ScrollToTopLink from './ScrollToTopLink.js'; 
-
-import Logo from '../photos/logo.png'
-
-import './css/Navbar.css'
-
-//<li><Link to="/info">Info</Link></li>
 function Test() {
+  const [isOpen, setIsOpen] = useState(false); // State to manage menu toggle
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="App">
-        <header className="header">
-          <div className="logo">
+      <header className="header">
+        <div className="logo">
           <ScrollToTopLink to="/">
-              <img src={Logo} alt="Logo" />
+            <img src={Logo} alt="Logo" />
           </ScrollToTopLink>
-          </div>
-          <nav className="menu">
+        </div>
+        <div className="menu-toggle" onClick={toggleMenu}>
+          <FaBars />
+        </div>
+        <nav className={isOpen ? "menu active" : "menu"}>
           <ul>
             <li><ScrollToTopLink to="/">Homepage</ScrollToTopLink></li>
             <li><ScrollToTopLink to="/aboutUs">About Us</ScrollToTopLink></li>
             <li><ScrollToTopLink to="/contact">Contact</ScrollToTopLink></li>
           </ul>
-          </nav>
+        </nav>
+        <div className="contact-info">
           <p className="blank"><FaEnvelope /> <a className="link" href="mailto:dinomatoruga17@gmail.com">dinomatoruga17@gmail.com</a></p>
           <p><FaPhone /> <a className="link" href="tel:+436607700543">+436607700543</a></p>
-          <div className="contact-info">
           <p><FaPhone /> <a className="link" href="tel:+436603135530">+436603135530</a></p>
-          </div>
-        </header>
-
-        
+        </div>
+      </header>
     </div>
   );
 }
