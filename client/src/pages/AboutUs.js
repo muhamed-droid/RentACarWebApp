b29fb1b85fe540f08201e4c492c0141348b7f875
muhamed-droid <mmasnopita1@etf.unsa.ba>
2024-05-13 16:03:23 +0200

    footer and about us modifications

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -1,53 +1,4 @@
-// src/components/AboutUs.js
-/*import React from 'react';
-import './css/AboutUs.css'; // Make sure to create this CSS file
-import Navbar from './Navbar';
-import Dino from '../photos/Dino.jpg';
-import Map from './Map.js';
-import AllRightsReserved from './AllRightsReserved.js';
-import ButtonToTop from './ButtonToTop.js';
-import Footer from './Footer.js';
-
-const AboutUs = () => {
-  return (
-    <div className="aboutUs">
-      <div className='navbar'>
-        <Navbar></Navbar>
-      </div>
-      <div> </div>
-      <div className="hero">
-        <h1 className="animated-title">Willkommen im KFZ Hampi</h1>
-        <p>Erfahren Sie mehr über unsere Mission, Vision und Werte.</p>
-      </div>
-
-      <div className="team">
-        <h2>Meet Our Team</h2>
-        <div className="team-members">
-          <div className="team-member">
-            <img src={Dino} alt="Team Member" className="team-member-img"/>
-            <h3>Dino</h3>
-            <p>CEO</p>
-          </div>
-          {/* Repeat for other team members }
-        </div>
-      </div>
-      <div className='google-map'>
-        <Map/>
-      </div>
-      <div>
-        <ButtonToTop/>
-      </div>
-      <div> 
-        <Footer/>
-      </div>
-      <div className='allRightsReserved'>
-        <AllRightsReserved/>
-      </div>
-    </div>
-  );
-};
-
-export default AboutUs; */ 
+ 
 
 import React from 'react';
 import './css/AboutUs.css';
@@ -67,6 +18,7 @@ function AboutUs() {
       <div>
         <Navbar/>
       </div>
+      <div className="about-us-content"></div>
       <div className="image-container">
       <img src={Image} alt="Logo" className="image"/>
       </div>
